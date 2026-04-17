0db0422644f900d7ce6aeb89d108e7627ec2515d
Hannan <hannansshah2004@gmail.com>
2026-04-17 19:47:40 -0400

    remove clerk

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,19 +1,8 @@
-import type { SignedInSessionResource } from "@clerk/types";
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-/**
- * Creates a Supabase client with the Clerk session token.
- * @param session - The session object from Clerk.
- * @returns A Supabase client.
- */
-export function createBrowserClient(session: SignedInSessionResource | null) {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
-    {
-      async accessToken() {
-        return session?.getToken() ?? null;
-      },
-    },
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
 }
