24c85e7319e61738c3e937b3538571b799b51999
Hannan <hannansshah2004@gmail.com>
2026-04-18 20:49:39 -0400

    added type for fda inspection

---
+++ types/fda_inspection_observations.ts
@@ -0,0 +1,9 @@
+export type fda_inspection_observations = {
+  id: string;
+  fiscal_year: number | null;
+  citation_number: string | null;
+  citation_desc: string | null;
+  observation_count: number | null;
+  program_area: string | null;
+  ingested_at: string;
+};
