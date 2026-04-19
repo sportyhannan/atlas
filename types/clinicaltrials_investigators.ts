fd1fed29bdee41b5097b174cb6cfc6bb52784ce1
Hannan <hannansshah2004@gmail.com>
2026-04-18 20:48:59 -0400

    added type for clinical trial investigators

---
+++ types/clinicaltrials_investigators.ts
@@ -0,0 +1,16 @@
+export type clinicaltrials_investigators = {
+  id: string;
+  nct_id: string | null;
+  full_name: string | null;
+  role: string | null;
+  affiliation: string | null;
+  facility_name: string | null;
+  city: string | null;
+  state: string | null;
+  country: string | null;
+  zip: string | null;
+  email: string | null;
+  phone: string | null;
+  resolved_investigator_id: string | null;
+  ingested_at: string;
+};
